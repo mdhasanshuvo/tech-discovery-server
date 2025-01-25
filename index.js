de19426b9d00ddde67fb4e7eecaf9d6ca0d6d174
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-01-26 04:35:34 +0600

    User data post api created by crosschecking previous existing or not

--- index.js
+++ index.js
@@ -35,7 +35,20 @@ async function run() {
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-        
+
+        const userCollection = client.db("Tech-Discovery-DB").collection("Users");
+
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "user already exists", insertedId: null });
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
 
 
     } finally {
