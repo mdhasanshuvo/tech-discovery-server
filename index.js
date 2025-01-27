a0f6aa8456edc1edf6d290c798c6c658889ade77
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2025-01-27 21:05:51 +0600

    created payment intent

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors({
     origin: [
@@ -238,7 +239,7 @@ async function run() {
             try {
                 const products = await productCollection
                     .find()
-                    .sort({ status: 1 }) 
+                    .sort({ status: 1 })
                     .toArray();
                 res.send(products);
             } catch (error) {
@@ -531,6 +532,22 @@ async function run() {
                 res.status(500).send({ message: 'Failed to fetch featured products', error });
             }
         });
+
+
+
+        // payment intent
+        app.post('/create-payment-intent', async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
 
 
 
